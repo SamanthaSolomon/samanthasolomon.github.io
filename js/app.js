a464f9736260078c65f73bd52a5cdb620c09b60a
Samantha Solomon <sleahsolomon@gmail.com>
2020-12-18 17:37:05 -0500

    data is set up

--- js/app.js
+++ js/app.js
@@ -1,1 +1,35 @@
-console.log("hello world");
+/////////////// PULL AND RENDER DATA FROM GOOGLE SHEETS ///////////////
+
+$.ajax("https://spreadsheets.google.com/feeds/list/11wvPAVvRzcKv7hORaWcsidISosUxtyNx0AuugxxMwyo/1/public/full?alt=json")
+.then((data) => {
+    //Checking My Data
+    console.log(data)
+
+    //put out projects in a variable
+    const rawProjects = data.feed.entry
+
+    //log our projects
+    console.log(rawProjects)
+
+    //Prettify our projects array
+    const projects = rawProjects.map((project) => {
+        return{
+            name: project.gsx$name.$t,
+            img: project.gsx$img.$t,
+            description: project.gsx$description.$t,
+            live: project.gsx$live.$t,
+            github: project.gsx$github.$t
+        }
+    })
+
+console.log(projects)
+///////////// USE JQUERY TO RENDER PROJECTS ON PAGE /////////////
+
+
+
+////////////////////////////////////////////////////////
+
+})
+
+
+///////////// PULL DATA FROM BLOG FROM HEADLESS CMS //////////////
