449cde2c745079bc5952c7e0c5a46806c07739e6
Samantha Solomon <sleahsolomon@gmail.com>
2020-12-24 15:49:39 -0500

    added card component

---
+++ js/components.js
@@ -0,0 +1,29 @@
+class MyCard extends HTMLElement{
+    constructor(){
+        super()
+
+        const name = this.getAttribute("name")
+        const img = this.getAttribute("img")
+        const description = this.getAttribute("description")
+        const live = this.getAttribute("live")
+        const github = this.getAttribute("github")
+ 
+        // console.log(name, img, description, live, github)
+        
+        this.innerHTML = `
+        <sl-card>
+        <h5>${name}</h5>
+        <img src=${img} alt=${name} slot="image">
+        <p>${description}<p>
+        <sl-button-group>
+            <a href=${live}<sl-button>Live</sl-button></a>
+            <a href=${github}<sl-button>GitHub</sl-button></a>
+        </sl-button-group>
+        
+        
+        </sl-card> `
+
+    }
+}
+
+customElements.define("my-card", MyCard)
