c58f6839f7d07a0e30893569899fc04b11fb0c2c
SamanthaSolomon <sleahsolomon@gmail.com>
2021-09-15 18:56:13 -0400

    airtable async await recieving data

--- js/app.js
+++ js/app.js
@@ -1,69 +1,99 @@
 // PULL AND RENDER DATA FROM AIRTABLE //
-const Airtable = require('airtable')
-require('dotenv').config()
-const { baseID, AIRTABLE_API_Key } = process.env
-// Airtable.configure({
-//     endpointUrl: 'https://api.airtable.com',
-//     apiKey: API_Key
+
+// require('dotenv').config()
+// const { baseID, AIRTABLE_API_Key } = process.env
+
+
+// const base = new Airtable({apiKey: AIRTABLE_API_Key}).base(baseID)
+
+
+var Airtable = require('airtable');
+var base = new Airtable({apiKey: 'keyGaHiDCegMafUfa'}).base('appyjIMa5kCCoyxje');
+
+const table = base('Sheet1')
+
+const getRecords = async () => {
+    const records = await table.select({
+        view: 'Grid view',
+        fields: ['name', 'img', 'description', 'live', 'github']
+    }).firstPage((err, records) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        records.forEach((record) => {
+            console.log('Retrieved', record.get('name', 'img', 'description', 'live', 'github'));
+        })
+    });
+}
+
+getRecords()
+
+// const prettify = records.map((record) => {
+//     return{
+//         id: record.id,
+//         fields: record.fields
+//     }
 // })
 
-const base = new Airtable({apiKey: AIRTABLE_API_Key}).base(baseID)
-console.log(baseID)
+// prettify()
 
-$.ajax()
-.then((data) => {
-    //Checking My Data
-    console.log('data-', data)
+//OLD AJAX CALL
 
-    //put projects in a variable
-    const rawProjects = data.feed.entry
+// $.ajax()
+// .then((data) => {
+//     //Checking My Data
+//     console.log('data-', data)
 
-    //log projects
-    console.log(rawProjects)
+//     //put projects in a variable
+//     const rawProjects = data.feed.entry
 
-    //Prettify projects array
-    const projects = rawProjects.map((project) => {
-        return{
-            name: project.gsx$name.$t,
-            img: project.gsx$img.$t,
-            description: project.gsx$description.$t,
-            live: project.gsx$live.$t,
-            github: project.gsx$github.$t
-        }
-    })
+//     //log projects
+//     console.log(rawProjects)
+
+//     //Prettify projects array
+//     const projects = rawProjects.map((project) => {
+//         return{
+//             name: project.gsx$name.$t,
+//             img: project.gsx$img.$t,
+//             description: project.gsx$description.$t,
+//             live: project.gsx$live.$t,
+//             github: project.gsx$github.$t
+//         }
+//     })
 
    // USE JQUERY TO RENDER PROJECTS ON PAGE //
-        for (i=0;i < projects.length; i++) {
-        const $div = $(
-            `<sl-card class="card-image">
-                <h5>${projects[i].name}</h5>
-                <img slot="image" src=${projects[i].img} 
-                alt="${projects[i].name}">
-                <p>${projects[i].description}</p>
-                    <sl-button class="live-button" href=${projects[i].live} target="_blank">See It Live</sl-button>
-                    ${
-                        projects[i].github !== "" ?
-                            `<sl-button class="gitHub-button" href=${projects[i].github} target="_blank">See GitHub</sl-button>`
-                        : "" 
-                    }        
-                </div>
-            </sl-card>`
-            )
-        $(".cards").append($div)
-    }
+//         for (i=0;i < projects.length; i++) {
+//         const $div = $(
+//             `<sl-card class="card-image">
+//                 <h5>${projects[i].name}</h5>
+//                 <img slot="image" src=${projects[i].img} 
+//                 alt="${projects[i].name}">
+//                 <p>${projects[i].description}</p>
+//                     <sl-button class="live-button" href=${projects[i].live} target="_blank">See It Live</sl-button>
+//                     ${
+//                         projects[i].github !== "" ?
+//                             `<sl-button class="gitHub-button" href=${projects[i].github} target="_blank">See GitHub</sl-button>`
+//                         : "" 
+//                     }        
+//                 </div>
+//             </sl-card>`
+//             )
+//         $(".cards").append($div)
+//     }
 
-})
+// })
 
 
 // NAV DRAWER //
 
-const drawer = document.querySelector('.drawer-placement-left')
+// const drawer = document.querySelector('.drawer-placement-left')
   
-  const openButton = drawer.nextElementSibling
+//   const openButton = drawer.nextElementSibling
   
-    const closeButton = $('.nav-button').on('click', () => {
-        drawer.hide()
-        })
+//     const closeButton = $('.nav-button').on('click', () => {
+//         drawer.hide()
+//         })
 
-  openButton.addEventListener('click', () => drawer.show())
+//   openButton.addEventListener('click', () => drawer.show())
 
