0bdfdb940319884ec9f4b823c6700d7f58245631
Samantha <sleahsolomon@gmail.com>
2022-09-13 15:09:48 -0400

    prettie config all pages, add doc-sample

--- js/data.js
+++ js/data.js
@@ -1,67 +1,84 @@
 const projects = [
-  { 
-    name : "Worn Out",
-    img : "https://res.cloudinary.com/dcvgmixhx/image/upload/v1615927359/WornOut/Wornout-closet.png",
-    description : "Fully responsive, clean user interface built with Svelte helps users easily view their wear patterns. Custom database constructed with Ruby on Rails allows users to store, update, and delete items. Allows users to track how often they wear items with a digital closet.",
-    live : "https://wornout.netlify.app/",
-    github : "https://github.com/SamanthaSolomon/worn_out_client"
+  {
+    name: "Documentation Writing Sample",
+    img: "https://res.cloudinary.com/dcvgmixhx/image/upload/v1663096054/docs-homepage_wwheej.png",
+    description:
+      "Developer docs written for SubscribePro’s Merchant App UDFs Feature. Written in Markdown, deployed/built with Gatsby",
+    live: "https://samanthasolomon.com/documentation-samples/",
+    github: "https://github.com/SamanthaSolomon/documentation-samples",
   },
-  { 
-    name : "Get A Dad Joke",
-    img : "https://res.cloudinary.com/dcvgmixhx/image/upload/v1611538227/Portfolio%20-%20Projects/ss1_ycdt4c.png",
-    description : "This react app allows users to search for jokes or generate a random joke. It pulls data from the icanhazdadjoke API. CSS uses BEM and variables for efficiency. Mobile, tablet, and desktop responsive. Set state and fetched data are from two endpoints.",
-    live : "https://getadadjoke.netlify.app/",
-    github : "https://github.com/SamanthaSolomon/Get-A-Dad-Joke"
+  {
+    name: "Worn Out",
+    img: "https://res.cloudinary.com/dcvgmixhx/image/upload/v1615927359/WornOut/Wornout-closet.png",
+    description:
+      "Fully responsive, clean user interface built with Svelte helps users easily view their wear patterns. Custom database constructed with Ruby on Rails allows users to store, update, and delete items. Allows users to track how often they wear items with a digital closet.",
+    live: "https://wornout.netlify.app/",
+    github: "https://github.com/SamanthaSolomon/worn_out_client",
   },
-  { 
-    name : "Rooster Grin",
-    img : "https://res.cloudinary.com/dcvgmixhx/image/upload/v1624292637/Portfolio%20-%20Projects/Screen_Shot_2021-06-21_at_12.23.03_PM_web3xp.png",
-    description : "Technical assessment for Rooster Grin Media, serving the dentistry sector. HTML, Sass/Scss, CSS, JS, JQuery one-page app with a custom carousel. Desktop Only.",
-    live : "http://samanthasolomon.com/RoosterGrin/",
-    github : "https://github.com/SamanthaSolomon/RoosterGrin"
+  {
+    name: "Get A Dad Joke",
+    img: "https://res.cloudinary.com/dcvgmixhx/image/upload/v1611538227/Portfolio%20-%20Projects/ss1_ycdt4c.png",
+    description:
+      "This react app allows users to search for jokes or generate a random joke. It pulls data from the icanhazdadjoke API. CSS uses BEM and variables for efficiency. Mobile, tablet, and desktop responsive. Set state and fetched data are from two endpoints.",
+    live: "https://getadadjoke.netlify.app/",
+    github: "https://github.com/SamanthaSolomon/Get-A-Dad-Joke",
   },
-  { 
-    name : "What's the Weather?",
-    img : "https://res.cloudinary.com/dcvgmixhx/image/upload/v1620764353/Portfolio%20-%20Projects/weather-app.png",
-    description : "A one page React app that pulls data baised on zipcode queries from the OpenWeather API.",
-    live : "https://what-the-weather.netlify.app/",
-    github : "https://what-the-weather.netlify.app/"
+  {
+    name: "Rooster Grin",
+    img: "https://res.cloudinary.com/dcvgmixhx/image/upload/v1624292637/Portfolio%20-%20Projects/Screen_Shot_2021-06-21_at_12.23.03_PM_web3xp.png",
+    description:
+      "Technical assessment for Rooster Grin Media, serving the dentistry sector. HTML, Sass/Scss, CSS, JS, JQuery one-page app with a custom carousel. Desktop Only.",
+    live: "http://samanthasolomon.com/RoosterGrin/",
+    github: "https://github.com/SamanthaSolomon/RoosterGrin",
   },
-  { 
-    name : "This Site!",
-    img : "https://res.cloudinary.com/dcvgmixhx/image/upload/v1611537958/Portfolio%20-%20Projects/Screen_Shot_2021-01-24_at_8.24.34_PM_cmzycl.png",
-    description : "Using HTML, CSS, JS, and Shoelace I built this mobile, tablet, and desktop responsive site. The work section uses jQuery to pull data from a google spreadsheet so I can easily add or remove projects without editing the site directly. ",
-    live : "https://samanthasolomon.com/",
-    github : "https://github.com/SamanthaSolomon/samanthasolomon.github.io"
+  {
+    name: "What's the Weather?",
+    img: "https://res.cloudinary.com/dcvgmixhx/image/upload/v1620764353/Portfolio%20-%20Projects/weather-app.png",
+    description:
+      "A one page React app that pulls data baised on zipcode queries from the OpenWeather API.",
+    live: "https://what-the-weather.netlify.app/",
+    github: "https://what-the-weather.netlify.app/",
   },
-  { 
-    name : "Hustle Busters",
-    img : "https://res.cloudinary.com/dcvgmixhx/image/upload/v1635953061/Screen_Shot_2021-11-03_at_11.23.19_AM_rvp0je.png",
-    description : "The app uses React to create a Netlify deployment using the backend repository bd-hustle-busters as its API data.The app contains full CRUD functionality, utilizing get routes to generate lead tables, put and delete routes utilized by the buttons contained in each lead, and a create route to create a new user from the user registration form. ",
-    live : "https://hustlebusters.netlify.app/",
-    github : "https://github.com/gbarka2/fe-hustle-busters#hustle-busters---frontend"
+  {
+    name: "This Site!",
+    img: "https://res.cloudinary.com/dcvgmixhx/image/upload/v1611537958/Portfolio%20-%20Projects/Screen_Shot_2021-01-24_at_8.24.34_PM_cmzycl.png",
+    description:
+      "Using HTML, CSS, JS, and Shoelace I built this mobile, tablet, and desktop responsive site. The work section uses jQuery to pull data from a google spreadsheet so I can easily add or remove projects without editing the site directly. ",
+    live: "https://samanthasolomon.com/",
+    github: "https://github.com/SamanthaSolomon/samanthasolomon.github.io",
   },
-  { 
-    name : "Strong City Baltimore",
-    img : "https://res.cloudinary.com/dcvgmixhx/image/upload/v1608589105/Portfolio%20-%20Projects/storngcitybaltimore-blog.png",
-    description : "Complete redesign and overhaul of organization's website that reflects the new brand, better showcases work, and is more user friendly. Deisgned and built with the help of Web Interactive Technologies, Inc.",
-    live : "https://www.strongcitybaltimore.org/",
-    github : ""
+  {
+    name: "Hustle Busters",
+    img: "https://res.cloudinary.com/dcvgmixhx/image/upload/v1635953061/Screen_Shot_2021-11-03_at_11.23.19_AM_rvp0je.png",
+    description:
+      "The app uses React to create a Netlify deployment using the backend repository bd-hustle-busters as its API data.The app contains full CRUD functionality, utilizing get routes to generate lead tables, put and delete routes utilized by the buttons contained in each lead, and a create route to create a new user from the user registration form. ",
+    live: "https://hustlebusters.netlify.app/",
+    github:
+      "https://github.com/gbarka2/fe-hustle-busters#hustle-busters---frontend",
   },
-  { 
-    name : "Random Tarot Generator",
-    img : "https://res.cloudinary.com/dcvgmixhx/image/upload/v1608596792/Portfolio%20-%20Projects/viva-luna-studios-GLsAydqqgzs-unsplash_xoukup.jpg",
-    description : "Random tarot card generator created using JavaScript.",
-    live : "http://samanthasolomon.com/Random-Tarot-Generator/",
-    github : "https://github.com/SamanthaSolomon/tarot"
+  {
+    name: "Strong City Baltimore",
+    img: "https://res.cloudinary.com/dcvgmixhx/image/upload/v1608589105/Portfolio%20-%20Projects/storngcitybaltimore-blog.png",
+    description:
+      "Complete redesign and overhaul of organization's website that reflects the new brand, better showcases work, and is more user friendly. Deisgned and built with the help of Web Interactive Technologies, Inc.",
+    live: "https://www.strongcitybaltimore.org/",
+    github: "",
   },
-  { 
-    name : "Lord of the Rings Map",
-    img : "https://res.cloudinary.com/dcvgmixhx/image/upload/v1608597034/Portfolio%20-%20Projects/LOTR%20Map.png",
-    description : "Interactive Lord of the Rings map created using CSS, JS, and JQuery.",
-    live : "https://samanthasolomon.github.io/LOTR/",
-    github : "https://github.com/SamanthaSolomon/LOTR"
+  {
+    name: "Random Tarot Generator",
+    img: "https://res.cloudinary.com/dcvgmixhx/image/upload/v1608596792/Portfolio%20-%20Projects/viva-luna-studios-GLsAydqqgzs-unsplash_xoukup.jpg",
+    description: "Random tarot card generator created using JavaScript.",
+    live: "http://samanthasolomon.com/Random-Tarot-Generator/",
+    github: "https://github.com/SamanthaSolomon/tarot",
   },
-]
+  {
+    name: "Lord of the Rings Map",
+    img: "https://res.cloudinary.com/dcvgmixhx/image/upload/v1608597034/Portfolio%20-%20Projects/LOTR%20Map.png",
+    description:
+      "Interactive Lord of the Rings map created using CSS, JS, and JQuery.",
+    live: "https://samanthasolomon.github.io/LOTR/",
+    github: "https://github.com/SamanthaSolomon/LOTR",
+  },
+];
 
-export { projects }
+export { projects };
